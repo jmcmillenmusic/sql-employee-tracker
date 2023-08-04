c149efb50b13a24d44d0776e96e679827848ea6f
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-04 16:43:12 -0500

    Almost finished setting up SQL query to return all employee information

--- db/querytests.sql
+++ db/querytests.sql
@@ -2,6 +2,12 @@
 -- SELECT * FROM department;
 
 -- View All Roles
-SELECT role.id AS id, role.title AS Title, role.salary AS Salary, role.department_id AS Dept_ID, department.name AS Dept_Name
-FROM role
+-- SELECT role.id AS id, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
+-- FROM role
+-- JOIN department ON role.department_id = department.id;
+
+-- View All Employees
+SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, department.name AS Dept_Name, role.salary AS Salary, employee.manager_id AS Mgr_First_Name, employee.manager_id AS Mgr_Last_Name
+FROM employee
+JOIN role ON employee.role_id = role.id
 JOIN department ON role.department_id = department.id;
