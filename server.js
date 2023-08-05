072d6f6f1c49bdda355c02eab44bdccafd2c1c7a
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-04 23:59:35 -0500

    Created viewAllRoles() and viewAllEmployees() functions

--- server.js
+++ server.js
@@ -34,6 +34,28 @@ function viewAllDepartments() {
   });
 };
 
+// db.query function to view all roles
+function viewAllRoles() {
+  db.query('SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name FROM role JOIN department ON role.department_id = department.id', function (err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(results);
+    }
+  });
+};
+
+// db.query function to view all employees
+function viewAllEmployees() {
+  db.query('SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, department.name AS Dept_Name, role.salary AS Salary, CONCAT(manager.first_name, " ", manager.last_name) AS Manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id AND CASE WHEN employee.id != manager.id THEN true ELSE false END', function (err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(results);
+    }
+  });
+};
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
