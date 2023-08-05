ba41a3ba05622fe3a688c2a9f528a5d3f87bac80
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-04 22:46:03 -0500

    Added SQL query to add a role and return the full list of roles with name, salary, and department name

--- db/querytests.sql
+++ db/querytests.sql
@@ -2,9 +2,9 @@
 -- SELECT * FROM department;
 
 -- View All Roles
--- SELECT role.id AS id, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
--- FROM role
--- JOIN department ON role.department_id = department.id;
+SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
+FROM role
+JOIN department ON role.department_id = department.id;
 
 -- View All Employees
 -- SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, department.name AS Dept_Name, role.salary AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
@@ -14,6 +14,13 @@
 -- LEFT JOIN employee manager ON employee.manager_id = manager.id AND CASE WHEN employee.id != manager.id THEN true ELSE false END;
 
 -- Add a department
-INSERT INTO department (name)
-VALUES ("Human Resources");
-SELECT * FROM department;
+-- INSERT INTO department (name)
+-- VALUES ("Human Resources");
+-- SELECT * FROM department;
+
+-- Add a role
+INSERT INTO role (title, salary, department_id)
+VALUES ("HR Specialist", 42500, 4);
+SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
+FROM role
+JOIN department ON role.department_id = department.id;
