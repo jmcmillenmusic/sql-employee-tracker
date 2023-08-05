a9d7911c12cd52aefff66f728341529e9080158b
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-04 23:11:31 -0500

    Added SQL quert to update an employee's role and return the full list of employees with names and titles

--- db/querytests.sql
+++ db/querytests.sql
@@ -13,22 +13,30 @@
 -- LEFT JOIN department ON role.department_id = department.id
 -- LEFT JOIN employee manager ON employee.manager_id = manager.id AND CASE WHEN employee.id != manager.id THEN true ELSE false END;
 
--- Add a department
+-- Add a Department
 -- INSERT INTO department (name)
 -- VALUES ("Human Resources");
 -- SELECT * FROM department;
 
--- Add a role
+-- Add a Role
 -- INSERT INTO role (title, salary, department_id)
 -- VALUES ("HR Specialist", 42500, 4);
 -- SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
 -- FROM role
 -- JOIN department ON role.department_id = department.id;
 
--- Add an employee
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Gina", "Tribbiani", 7, NULL);
-SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+-- Add an Employee
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES ("Gina", "Tribbiani", 7, NULL);
+-- SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+-- FROM employee
+-- LEFT JOIN role ON employee.role_id = role.id
+-- LEFT JOIN employee manager ON employee.manager_id = manager.id AND CASE WHEN employee.id != manager.id THEN true ELSE false END;
+
+-- Update an Employee's Role
+UPDATE employee
+SET role_id = 2
+WHERE id = 7;
+SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title
 FROM employee
-LEFT JOIN role ON employee.role_id = role.id
-LEFT JOIN employee manager ON employee.manager_id = manager.id AND CASE WHEN employee.id != manager.id THEN true ELSE false END;
+LEFT JOIN role ON employee.role_id = role.id;
