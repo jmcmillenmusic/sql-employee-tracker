2f11f3857591b9654049e1fc28efee622df050ae
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-04 22:27:43 -0500

    Added SQL query to add a department and return the full list of departments

--- db/querytests.sql
+++ db/querytests.sql
@@ -7,9 +7,13 @@
 -- JOIN department ON role.department_id = department.id;
 
 -- View All Employees
-SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, department.name AS Dept_Name, role.salary AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
-FROM employee
-LEFT JOIN role ON employee.role_id = role.id
-LEFT JOIN department ON role.department_id = department.id
-LEFT JOIN employee manager ON employee.manager_id = manager.id AND CASE WHEN employee.id != manager.id THEN true ELSE false END;
+-- SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, department.name AS Dept_Name, role.salary AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+-- FROM employee
+-- LEFT JOIN role ON employee.role_id = role.id
+-- LEFT JOIN department ON role.department_id = department.id
+-- LEFT JOIN employee manager ON employee.manager_id = manager.id AND CASE WHEN employee.id != manager.id THEN true ELSE false END;
 
+-- Add a department
+INSERT INTO department (name)
+VALUES ("Human Resources");
+SELECT * FROM department;
