af4e75409a75b1e25289e860f0053b971d6ef2bc
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-04 23:00:12 -0500

    Added SQL query to add an employee and return the full list of employees with names, titles, and managers

--- db/querytests.sql
+++ db/querytests.sql
@@ -2,9 +2,9 @@
 -- SELECT * FROM department;
 
 -- View All Roles
-SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
-FROM role
-JOIN department ON role.department_id = department.id;
+-- SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
+-- FROM role
+-- JOIN department ON role.department_id = department.id;
 
 -- View All Employees
 -- SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, department.name AS Dept_Name, role.salary AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
@@ -19,8 +19,16 @@ JOIN department ON role.department_id = department.id;
 -- SELECT * FROM department;
 
 -- Add a role
-INSERT INTO role (title, salary, department_id)
-VALUES ("HR Specialist", 42500, 4);
-SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
-FROM role
-JOIN department ON role.department_id = department.id;
+-- INSERT INTO role (title, salary, department_id)
+-- VALUES ("HR Specialist", 42500, 4);
+-- SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Dept_Name
+-- FROM role
+-- JOIN department ON role.department_id = department.id;
+
+-- Add an employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Gina", "Tribbiani", 7, NULL);
+SELECT employee.id AS id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN employee manager ON employee.manager_id = manager.id AND CASE WHEN employee.id != manager.id THEN true ELSE false END;
